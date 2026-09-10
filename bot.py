ac0953362b7223823408fab5c0d6320c16c8d5e3
Legolas <komiljonovbehruz96@gmail.com>
2026-09-10 22:53:27 +0500

    Changed main function

--- bot.py
+++ bot.py
@@ -235,15 +235,6 @@ async def handle(request):
     return web.Response(text="Bot 24/7 faol ishlamoqda!")
 
 async def main():
-    # Kichik veb-server yaratish (Render port topishi uchun)
-    app = web.Application()
-    app.router.add_get("/", handle)
-    runner = web.AppRunner(app)
-    await runner.setup()
-    port = int(os.environ.get("PORT", 8080))
-    site = web.TCPSite(runner, "0.0.0.0", port)
-    await site.start()
-
     print("Bot va Admin Panel ishga tushdi...")
     await dp.start_polling(bot)
 
